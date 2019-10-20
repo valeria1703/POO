023482980e64c5233cee65bb4fd8f77a6e6822d6
Valeria Luis <54416927+valeria733@users.noreply.github.com>
2019-10-19 17:11:09 -0700

    Add files via upload

---
+++ PARCIAL 2/bici/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace bici
+{
+    class Bici
+    {
+        private string marca;
+        private int current_velocity, velocities;
+        public void print()
+        {
+            Console.WriteLine("Marca: {0}", marca);
+            Console.WriteLine("Velocity: {0}", current_velocity);
+        }
+        public void init(string marca, int cv, int vs)
+        {
+            this.marca=marca;
+            current_velocity=cv;
+            velocities=vs;
+        }
+        public void sube()
+        {
+            current_velocity=current_velocity+1;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bici bici=new Bici();
+            bici.init("BiciMex", 2, 13);
+            bici.sube();
+            bici.print();
+        }
+    }
+}
