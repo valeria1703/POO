424c85ca47cf23b19f978bc3ff054c89d0de7cf9
Valeria Luis <54416927+valeria733@users.noreply.github.com>
2019-10-19 17:22:36 -0700

    Add files via upload

---
+++ PARCIAL 2/padre/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+namespace padre
+{
+    class Componente
+    {
+        public string nombre;
+        public Contenedor padre;
+
+        public Componente(string nombre, Contenedor padre)
+        {   
+               this.nombre = nombre;
+               this.padre = padre; 
+        } 
+        public void print(){
+            Console.WriteLine($"Soy { nombre } y mi padre es { padre.nombre }" );
+        }
+    }
+
+    class Contenedor{
+        public string nombre;
+        public List <Componente> componentes = new List <Componente> ();
+
+        public Contenedor(string nombre)
+        {
+            this.nombre=nombre;
+            componentes.Add(new Componente("primero", this ));
+            componentes.Add(new Componente("segundo", this ));
+            componentes.Add(new Componente("tercero", this ));
+        }
+
+        public void print()
+        {
+            foreach(var hijo in componentes)
+            {
+                hijo.print();
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Contenedor pa1 = new Contenedor("Contenedor 1");
+           Contenedor pa2 = new Contenedor("Contenedor 2");
+
+           pa1.print();
+           Console.WriteLine("__________________");
+           pa2.print();
+
+        }
+    }
+}
