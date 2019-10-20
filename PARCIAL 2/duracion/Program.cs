cfd9ecd58cc22a990ccd898c3590c492c0a83a21
Valeria Luis <54416927+valeria733@users.noreply.github.com>
2019-10-19 17:00:44 -0700

    Add files via upload

---
+++ PARCIAL 2/duracion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Duracion
+{
+    class Duracion
+    {
+        private int Horas;
+        private int Minutos;
+        private int Segundos;
+
+        public Duracion(int H, int M, int S)
+        {
+            Horas = H;
+            Minutos = M;
+            Segundos = S;
+        }    
+
+        public void print ()
+        {
+            Console.WriteLine(Horas + ":" + Minutos + ":" + Segundos);
+        }
+
+    }
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duracion Pelicula = new Duracion(2, 15, 12);
+            Duracion Cancion = new Duracion(0, 02, 15);
+            Duracion Partido = new Duracion(2, 00, 10);
+
+            Pelicula.print();
+            Cancion.print();
+            Partido.print();
+        }
+    }
+}
