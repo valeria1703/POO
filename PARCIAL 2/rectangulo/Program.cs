c2bdab74c352048bbba1672eb8e355cc6974af65
Valeria Luis <54416927+valeria733@users.noreply.github.com>
2019-10-20 13:38:10 -0700

    Add files via upload

---
+++ PARCIAL 2/rectangulo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace rectangulo
+{
+    class Rectangulo
+    {
+        private int bace, altura,ar;
+        public Rectangulo (int ba, int al)
+        {
+            this.bace=ba;
+            this.altura=al;
+            ar=ba*al;
+        }
+        public static Rectangulo operator + (Rectangulo a, Rectangulo l)
+        {
+            return new Rectangulo (a.ar + l.ar);
+        }
+        
+    }    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo are = new Rectangulo (10,5);
+            Rectangulo are1 = new Rectangulo (4,6);
+            Rectangulo r = are+are1;
+        }
+    }
+}
